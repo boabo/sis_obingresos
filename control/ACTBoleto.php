8b319f9185830f199fee38a0043af8fbcde163e1
franklinespinozaalvarez <franklin.espinoza.alvarez@gmail.com>
2018-12-11 17:43:36 -0400

    cambio en la url TraerTkt skbpruebas a skbproduccion

--- control/ACTBoleto.php
+++ control/ACTBoleto.php
@@ -964,7 +964,7 @@ class ACTBoleto extends ACTbase{
         $json_data = json_encode($data);
 
         $s = curl_init();
-        curl_setopt($s, CURLOPT_URL, 'http://skbpruebas.cloudapp.net/ServicioINT/ServicioInterno.svc/TraerTkt');
+        curl_setopt($s, CURLOPT_URL, 'http://skbproduccion.cloudapp.net/ServicioINT/ServicioInterno.svc/TraerTkt');
         curl_setopt($s, CURLOPT_POST, true);
         curl_setopt($s, CURLOPT_POSTFIELDS, $json_data);
         curl_setopt($s, CURLOPT_RETURNTRANSFER, true);
