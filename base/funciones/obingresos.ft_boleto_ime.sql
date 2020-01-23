418028e7f23dc0cdeed7f8fcd18647941908e752
ismael2019 <irva2008_@hotmail.com>
2020-01-23 18:27:42 -0400

    Actualizar viajero frecuento si ya consulto el counter (Ismael Valdivia)

--- base/funciones/obingresos.ft_boleto_ime.sql
+++ base/funciones/obingresos.ft_boleto_ime.sql
@@ -158,6 +158,7 @@ DECLARE
     v_tolerancia			numeric;
 
     v_codigo_moneda				varchar;
+    v_consultado			integer;
 BEGIN
 
     v_nombre_funcion = 'obingresos.ft_boleto_ime';
@@ -2803,6 +2804,23 @@ BEGIN
 
             /***************************Aumentando para alimentar en la tabla tconsulta_viajero_frecuente (Ismael Valdivia 19/12/2019)****************************/
             if (v_id_viajero_frecuente is not null) then
+
+              /*Poniendo este control para ir actualizando si el cajero realiza el canjeado Ismael Valdivia (23/01/2020)*/
+              select count (*) into v_consultado
+              from obingresos.tconsulta_viajero_frecuente via
+              where ffid = v_parametros.ffid and voucher_code = v_parametros.voucherCode;
+
+
+              if (v_consultado >= 1) then
+              	update obingresos.tconsulta_viajero_frecuente set
+                message_canjeado = 'Canjeado por Caja en el punto de venta:'||v_nombre_punto_venta||' por el cajero: '||v_cajero,
+            	status_canjeado = 'OK',
+                nro_boleto = v_parametros.ticketNumber,
+                pnr = v_parametros.pnr,
+                estado = 'Canjeado'
+                where ffid = v_parametros.ffid and voucher_code = v_parametros.voucherCode;
+              end if;
+              /**********************************************************************************/
               insert into obingresos.tconsulta_viajero_frecuente(
                   ffid,
                   estado_reg,
@@ -3030,6 +3048,7 @@ LANGUAGE 'plpgsql'
 VOLATILE
 CALLED ON NULL INPUT
 SECURITY INVOKER
+PARALLEL UNSAFE
 COST 100;
 
 ALTER FUNCTION obingresos.ft_boleto_ime (p_administrador integer, p_id_usuario integer, p_tabla varchar, p_transaccion varchar)
