adcb76a923a0168dec8938346e821302753b6332
ismael2019 <irva2008_@hotmail.com>
2021-07-08 14:05:31 -0400

    Mostrando description en reporte

--- base/funciones/obingresos.ft_reporte_cuenta_sel.sql
+++ base/funciones/obingresos.ft_reporte_cuenta_sel.sql
@@ -214,6 +214,7 @@ BEGIN
                     mo.pnr,
                     mo.tipo,
                     mo.fecha,
+                    mo.autorizacion__nro_deposito,
                       sum(case when mo.ajuste = 'no' and  mo.garantia = 'no'   then
                               (case when mo.id_moneda = 1 then
                                               mo.monto
@@ -252,7 +253,7 @@ BEGIN
                        mo.estado_reg = 'activo' and
                        mo.tipo = 'credito'
                        and mo.id_agencia = v_parametros.id_agencia
-                      group by mo.id_agencia, mo.fecha, mo.pnr, mo.tipo
+                      group by mo.id_agencia, mo.fecha, mo.pnr, mo.tipo, mo.autorizacion__nro_deposito
 
                       union all
 
@@ -261,6 +262,7 @@ BEGIN
                       			''::varchar as pnr,
                                	mo.tipo,
                                 mo.fecha,
+                                mo.autorizacion__nro_deposito,
                                 (case when mo.id_moneda = 1 then
                             	mo.monto
                            		 else
@@ -282,13 +284,15 @@ BEGIN
                  insert into	temp (
                                         tipo_credito,
                                         fecha,
-                                        importe
+                                        importe,
+                                        autorizacion__nro_deposito
                                         --garantia
                                         )
                                         values(
                                         v_creditos.tipo,
                                         v_creditos.fecha,
-                                        v_creditos.monto_credito
+                                        v_creditos.monto_credito,
+                                        v_creditos.autorizacion__nro_deposito
                  						--v_creditos.garantia
                                         );
 
