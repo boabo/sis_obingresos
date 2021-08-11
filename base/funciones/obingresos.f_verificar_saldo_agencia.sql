507f6247c06c7a160844a9eca353e2c010618183
ismael2019 <irva2008_@hotmail.com>
2021-08-11 12:05:32 -0400

    Control para postpago

--- base/funciones/obingresos.f_verificar_saldo_agencia.sql
+++ base/funciones/obingresos.f_verificar_saldo_agencia.sql
@@ -44,12 +44,23 @@ DECLARE
 
     v_terciariza				varchar;
     v_monto						numeric;
+    /*Auementando variables Ismael Valdivia (11/08/2021)*/
+    v_tipo_agencia				varchar;
+    v_validar_boleta			varchar;
+    v_tipo_pago					varchar;
 
 BEGIN
     v_nombre_funcion:='obingresos.f_verificar_saldo_agencia';
     po_saldo = obingresos.f_get_saldo_agencia(p_id_agencia,p_moneda);
 
-    select coalesce (a.terciariza,'no') into v_terciariza
+    select coalesce (a.terciariza,'no'),
+    		a.tipo_agencia,
+            a.validar_boleta,
+            a.tipo_pago
+     into v_terciariza,
+     	  v_tipo_agencia,
+          v_validar_boleta,
+          v_tipo_pago
     from obingresos.tagencia a
     where a.id_agencia = p_id_agencia;
 
@@ -86,10 +97,11 @@ BEGIN
 	else
 
           v_id_moneda_base = (select param.f_get_moneda_base());
-          if (po_saldo >= v_monto ) then
-              po_autorizacion = uuid_generate_v4()::varchar;
-              po_saldo = po_saldo - v_monto;
-              if (p_insertar = 'si')then
+
+          /*Auemntando condicion para omitir control de saldo (Ismael Valdivia 11/08/2021)*/
+          if (v_tipo_agencia = 'corporativa' and v_tipo_pago = 'postpago' and v_validar_boleta = 'no') THEN
+
+          	if (p_insertar = 'si')then
 
                   INSERT INTO
                       obingresos.tmovimiento_entidad
@@ -130,11 +142,64 @@ BEGIN
                       (case when v_terciariza = 'si' then p_monto_total - p_monto else NULL END)
                     );
                   end if;
+
           else
-              raise exception 'La agencia no tiene saldo suficiente para emitir el boleto. El saldo es de % %',po_saldo,v_moneda;
+
+              if (po_saldo >= v_monto ) then
+                po_autorizacion = uuid_generate_v4()::varchar;
+                po_saldo = po_saldo - v_monto;
+                if (p_insertar = 'si')then
+
+                    INSERT INTO
+                        obingresos.tmovimiento_entidad
+                      (
+                        id_usuario_reg,
+                        id_usuario_ai,
+                        usuario_ai,
+                        tipo,
+                        pnr,
+                        fecha,
+                        apellido,
+                        monto,
+                        id_moneda,
+                        autorizacion__nro_deposito,
+                        garantia,
+                        ajuste,
+                        id_periodo_venta,
+                        id_agencia,
+                        monto_total,
+                        comision_terciarizada
+                      )
+                      VALUES (
+                        p_id_usuario,
+                        NULL,
+                        NULL,
+                        'debito',
+                        p_pnr,
+                        p_fecha,
+                        p_apellido,
+                        v_monto,
+                        v_id_moneda,
+                        po_autorizacion,
+                        'no',
+                        'no',
+                        NULL,
+                        p_id_agencia,
+                        p_monto_total,
+                        (case when v_terciariza = 'si' then p_monto_total - p_monto else NULL END)
+                      );
+                    end if;
+            else
+                raise exception 'La agencia no tiene saldo suficiente para emitir el boleto. El saldo es de % %',po_saldo,v_moneda;
+            end if;
+
           end if;
 
-end if;
+
+
+
+
+   end if;
 
 	return;
 
@@ -153,3 +218,6 @@ VOLATILE
 CALLED ON NULL INPUT
 SECURITY INVOKER
 COST 100;
+
+ALTER FUNCTION obingresos.f_verificar_saldo_agencia (p_id_agencia integer, p_monto numeric, p_moneda varchar, p_id_usuario integer, p_pnr varchar, p_apellido varchar, p_insertar varchar, p_monto_total numeric, p_fecha date, out po_autorizacion varchar, out po_saldo numeric)
+  OWNER TO postgres;
